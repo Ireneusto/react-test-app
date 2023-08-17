e393bce61ff4b2bcf54a03a9138f1fceb9db2ae8
Ireneusto <ireneusto@gmail.com>
2023-08-17 11:16:59 +0200

    initialize

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Start() {
+function LegoList() {
     return (
         <div>
-            <div>
-                <h2>My first component</h2>
-                <h2>My second component</h2>
-            </div>
-            <div>
-                <h2>My first component</h2>
-                <h2>My second component</h2>
-            </div>
+            <Set />
+            <Set />
+            <Set />
+            <Set />
         </div>
     );
 }
+
+const Set = () => {
+    return (
+        <article>
+            <Image />
+            <Number />
+            <Name />
+        </article>
+    );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Number = () => <h2>Number</h2>;
+const Name = () => <h4>Name</h4>;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
